41b7d7665e4e06956089de206580b8f2af2947a1
Gunjan Naraniya <gnnaraniya@gmail.com>
2026-03-17 23:55:44 +0530

    saved user data collected from Signup Form into database

--- src/main/java/com/scm/controllers/PageController.java
+++ src/main/java/com/scm/controllers/PageController.java
@@ -1,11 +1,22 @@
 package com.scm.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.scm.entities.User;
+import com.scm.forms.UserForm;
+import com.scm.services.UserService;
 
 
 @Controller
 public class PageController {
+
+    @Autowired
+    private UserService userService;
     
     @GetMapping("/")
     public String homePage(){
@@ -38,8 +49,26 @@ public class PageController {
     }
 
     @GetMapping("/register")
-    public String register(){
+    public String register(Model modle){
+        UserForm userForm = new UserForm();
+        modle.addAttribute("userForm", userForm);
         System.out.println("Signup Page");
         return "register";
+    }
+
+    // processing user registration
+    @PostMapping("/register-user")
+    public String registerUser(@ModelAttribute UserForm userForm){
+
+        // User user = User.builder().name(userForm.getName()).email(userForm.getEmail()).password(userForm.getPassword()).about(userForm.getAbout()).phoneNumber(userForm.getPhoneNumber()).profilePic("").build();
+        User user = new User();
+        user.setName(userForm.getName());
+        user.setEmail(userForm.getEmail());
+        user.setPassword(userForm.getPassword());
+        user.setAbout(userForm.getAbout());
+        user.setPhoneNumber(userForm.getPhoneNumber());
+        user.setProfilePic("");
+        User savedUser = userService.saveUser(user);
+        return "redirect:/register";
     }
 }
---
+++ src/main/java/com/scm/services/impl/UserServiceImpl.java
@@ -0,0 +1,84 @@
+package com.scm.services.impl;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.scm.entities.User;
+import com.scm.helpers.ResourceNotFoundException;
+import com.scm.repositories.UserRepository;
+import com.scm.services.UserService;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Override
+    public User saveUser(User user) {
+        // user Id : have to be generated
+        String userid = UUID.randomUUID().toString();
+        user.setUserId(userid);
+        return userRepository.save(user);
+    }
+
+    @Override
+    public Optional<User> getUserById(String id) {
+        return userRepository.findById(id);
+    }
+
+    @Override
+    public Optional<User> updateUser(User user) {
+        User user2 = userRepository.findById(user.getUserId()).orElseThrow(() -> new ResourceNotFoundException("User not found"));
+        user2.setName(user.getName());
+        user2.setEmail(user.getEmail());
+        user2.setPassword(user.getPassword());
+        user2.setAbout(user.getAbout());
+        user2.setPhoneNumber(user.getPhoneNumber());
+        user2.setProfilePic(user.getProfilePic());
+        user2.setEnabled(user.isEnabled());
+        user2.setEmailVerified(user.isEmailVerified());
+        user2.setPhoneVerified(user.isPhoneVerified());
+        user2.setProvider(user.getProvider());
+        user2.setProviderUserId(user.getProviderUserId());
+
+        // save updated user in database
+        User updatedUser = userRepository.save(user2);
+
+        return Optional.ofNullable(updatedUser);
+        
+    }
+
+    @Override
+    public void deleteUser(String id) {
+        User user2 = userRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("User not found"));
+        userRepository.delete(user2);
+    }
+
+    @Override
+    public boolean isUserExist(String userId) {
+        User user2 = userRepository.findById(userId).orElse(null);
+        return user2 !=null ? true : false;
+
+    }
+
+    @Override
+    public boolean isUserExistByEmail(String email) {
+        User user = userRepository.findByEmail(email).orElse(null);
+        return user !=null ? true : false;
+    }
+
+    @Override
+    public List<User> getAllUsers() {
+        return userRepository.findAll();    
+    }
+
+}
